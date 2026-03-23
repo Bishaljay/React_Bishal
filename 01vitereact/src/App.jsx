d70fb703ce0c2345d313dacbec93388065f8575d
bishaljaysawal <jayravi077@gmail.com>
2026-03-24 01:02:32 +0530

    react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,6 @@
 
-<<<<<<< HEAD
 
 import Chai from "./chai"
-=======
-import Chai from "./Chai"
->>>>>>> 170954c9c245346bc4fb63c5181f66088b4eb255
-
 
 function App() {
   const username = "chai aur code"
